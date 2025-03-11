bcee4643647c1069c0f5662022effe719479fafc
Bryan Gross <bryangross21@ksu.edu>
2025-03-10 19:44:54 -0500

    Duck Pond Mini-game assets

---
+++ CIS598Project/Collisions/CollisionHelper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+
+namespace BalloonWorld.Collisions
+{
+	public static class CollisionHelper
+	{
+
+		/// <summary>
+		/// Detects collision between two bounding rectangles
+		/// </summary>
+		/// <param name="a">the first rectangle</param>
+		/// <param name="b">the second rectangle</param>
+		/// <returns>True if there is a collision, false if there isn't one</returns>
+		public static bool Collides(BoundingRectangle a, BoundingRectangle b) 
+		{
+			return !(a.Right < b.Left || a.Left > b.Right || a.Top > b.Bottom || a.Bottom < b.Top);
+		}
+	}
+}
--- CIS598Project/obj/Debug/net8.0-windows/CIS598Project.AssemblyInfo.cs
+++ CIS598Project/obj/Debug/net8.0-windows/CIS598Project.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CIS598Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+647c195a3d138d995ca38dd45e3e515bf60d31fa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f6f5bd525c068019b3b03c114606c4095086ca12")]
 [assembly: System.Reflection.AssemblyProductAttribute("CIS598Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CIS598Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
