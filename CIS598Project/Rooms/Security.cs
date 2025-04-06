13178f5e84504beafda612d6e35f2562dd1ab5e8
Bryan Gross <bryangross21@ksu.edu>
2025-04-06 17:40:46 -0500

    Security done

--- CIS598Project/Rooms/Security.cs
+++ CIS598Project/Rooms/Security.cs
@@ -30,9 +30,11 @@ namespace CIS598Project.Rooms
 		Start = 0,
 		Play,
 		Guess,
+		Results,
 		Intermission,
 		GameOver,
-		Win
+		Win,
+		Secret
 	}
 	public class Security : GameScreen
 	{
@@ -58,7 +60,7 @@ namespace CIS598Project.Rooms
 
 		Texture2D[] Screens = new Texture2D[3];
 
-		Texture2D[] signs = new Texture2D[2];
+		Texture2D[] signs = new Texture2D[3];
 
 		Texture2D[] Fred = new Texture2D[6];
 
@@ -75,10 +77,19 @@ namespace CIS598Project.Rooms
 
 		SoundEffect failed;
 
+		SoundEffect boo;
+
+		SoundEffect yay;
+
 		SoundEffect Crash;
+
+		SoundEffect horn;
 
 		SoundEffectInstance crashing;
 
+		SoundEffectInstance result;
+
+		SoundEffectInstance airHorn;
 
 		BoundingRectangle increase;
 
@@ -95,6 +106,8 @@ namespace CIS598Project.Rooms
 		bool crash = false;
 
 		bool end = false;
+
+		bool fail = false;
 
 		bool[] flashed = new bool[9];
 
@@ -117,11 +130,12 @@ namespace CIS598Project.Rooms
 		bool hasChildTouchedDoor = false;
 
 
-		string[] ourple = { "Hello there,\nwhat's wrong?", "Hmm? You don't want to\ntalk? Why not?", "Your parents say you\ncan't talk to strangers?", "Well they are smart, you \nshouldn't talk to strangers.", "However, you want to \nknow something cool?", "I'm not a stranger,\nI'm a friend of yours.", "Okay, you'll talk.\nThat's great!", "What was your problem...", "Hmm, none of your friends showed\nup for your birthday?", "Well, who needs them?\nI'm your friend and I know \njust how to celebrate your birthday.", "Just follow me and I'll take you\nto a special party room just for you." };
+		string[] ourple = { "Hi there, I noticed that\nyou just beat this game.", "You know what we do for\nguests that beat this game?", "We give them a special badge\nsince you know you're now a \npart of our security team!", "Hmm?", "Right, Freddy's Security Team!", "All we need to make you a part\nof the team is that badge.", "Follow me and I'll\nget you that badge." };
 
 		int ourpleCount = 0;
 
 		bool purpleDialogue = false;
+
 
 		double ourpleTimer = 0;
 
@@ -131,6 +145,8 @@ namespace CIS598Project.Rooms
 
 		double textTimer = 0;
 
+		double resultsTimer = 0;
+
 		int chosenChildPosition = 0;
 
 		int guess = 0;
@@ -138,6 +154,8 @@ namespace CIS598Project.Rooms
 		bool showText = false;
 
 		int currentRound = 1;
+
+		TimeSpan currentPosition;
 
 		private MouseState pastMousePosition;
 		private MouseState currentMousePosition;
@@ -151,6 +169,7 @@ namespace CIS598Project.Rooms
 		BoundingRectangle mouse = new(0, 0, 64, 64);
 		BoundingRectangle increaseBox;
 		BoundingRectangle decreaseBox;
+		BoundingRectangle GO;
 
 
         int score = 0;
@@ -232,21 +251,28 @@ namespace CIS598Project.Rooms
 			background[1] = _content.Load<Texture2D>("Security/Textures/Backgrounds/Camera_on");
 			signs[0] = _content.Load <Texture2D>("Security/Textures/Misc/Increase");
 			signs[1] = _content.Load<Texture2D>("Security/Textures/Misc/Decrease");
+			signs[2] = _content.Load<Texture2D>("Security/Textures/Misc/Go");
 			selection = _content.Load<SoundEffect>("Security/Sounds/Soundeffects/increaseDecrease");
 			nextRound = _content.Load<SoundEffect>("Security/Sounds/Soundeffects/nextround");
 			failed = _content.Load<SoundEffect>("Security/Sounds/Soundeffects/yourdone");
 			Crash = _content.Load<SoundEffect>("Duck_Pond/Duck_Pond_Sounds/df");
 			crashScreen = _content.Load<Texture2D>("Duck_Pond/Crash");
 			font = _content.Load<SpriteFont>("MiniGame_Font");
+			horn = _content.Load<SoundEffect>("Balloon_Barrel/Sounds/Soundeffect/win");
 			backgroundMusic = _content.Load<Song>("Security/Sounds/Music/Alchemists_Fantasy");
 
 			Screens[0] = _content.Load<Texture2D>("Fruity_Maze/Textures/Screen/scanlines");
 			Screens[1] = _content.Load<Texture2D>("Fruity_Maze/Textures/Screen/Screen");
 			Screens[2] = _content.Load<Texture2D>("Fruity_Maze/Textures/Screen/ScreenBorder");
 
+			boo = _content.Load<SoundEffect>("Security/Sounds/Soundeffects/boo");
+			yay = _content.Load<SoundEffect>("Security/Sounds/Soundeffects/yay");
+
+
 			childPosition = new Vector2(0, game.GraphicsDevice.Viewport.Height / 2);
 			increaseBox = new(game.GraphicsDevice.Viewport.Width / 2 + 150, game.GraphicsDevice.Viewport.Height / 2, 128, 128);
 			decreaseBox = new(game.GraphicsDevice.Viewport.Width / 2 + 150, game.GraphicsDevice.Viewport.Height / 2 + 150, 128, 128);
+			GO = new(game.GraphicsDevice.Viewport.Width / 2 + 350, game.GraphicsDevice.Viewport.Height / 2 + 100, 128, 128);
 
 			for (int i = 5; i >= 0; i--) 
 			{
@@ -328,7 +354,7 @@ namespace CIS598Project.Rooms
 
 				if (childPosition.X < game.GraphicsDevice.Viewport.Width - 249 && hasChildTouchedDoor == false)
 				{
-					childPosition.X += 10 + (currentRound * 2.5f);
+					childPosition.X += 5f + (currentRound * 2.5f);
 				}
 
 				if (childPosition.X >= game.GraphicsDevice.Viewport.Width - 249 && hasChildTouchedDoor == false)
@@ -338,23 +364,56 @@ namespace CIS598Project.Rooms
 
 				if (hasChildTouchedDoor == true)
 				{
-					childPosition.X -= (10 + currentRound * 2.5f);
+					childPosition.X -= 5f + currentRound * 2.5f;
 				}
 
 				if (childCounter == children[currentRound - 1].Length)
 				{
 					gameState = GameStateSecurity.Guess;
+					childCounter = 0;
 				}
 			}
 			else if (gameState == GameStateSecurity.Guess) 
 			{
 				if (mouse.collidesWith(increaseBox))
 				{
-
+					if (currentMousePosition.LeftButton == ButtonState.Pressed && pastMousePosition.LeftButton == ButtonState.Released)
+					{
+						guess++;
+						if (guess >= children[currentRound - 1].Length)
+						{
+							guess = 0;
+						}
+						selection.Play();
+					}
 				}
-				else if (mouse.collidesWith(decreaseBox)) 
+				else if (mouse.collidesWith(decreaseBox))
 				{
-
+					if (currentMousePosition.LeftButton == ButtonState.Pressed && pastMousePosition.LeftButton == ButtonState.Released)
+					{
+						guess--;
+						if (guess <= 0)
+						{
+							guess = children[currentRound - 1].Length - 1;
+						}
+						selection.Play();
+					}
+				}
+				else if (mouse.collidesWith(GO)) 
+				{
+					if (currentMousePosition.LeftButton == ButtonState.Pressed && pastMousePosition.LeftButton == ButtonState.Released)
+					{
+						currentPosition = MediaPlayer.PlayPosition;
+						MediaPlayer.Stop();
+						selection.Play();
+						if (chosenChildPosition != guess) 
+						{
+							fail = true;
+						}
+						chosenAnimationFrame = 0;
+						guess = 0;
+						gameState = GameStateSecurity.Results;
+					}
 				}
 			}
 
@@ -394,11 +453,16 @@ namespace CIS598Project.Rooms
 					}
 					if (showText)
 					{
-						spriteBatch.DrawString(font, "GUESSING TIME!!! " + chosenChildPosition , new Vector2(graphics.Viewport.Width / 2 - 250, 50), Color.White);
+						spriteBatch.DrawString(font, "GUESSING TIME!!!\n" + (chosenChildPosition + 1) , new Vector2(graphics.Viewport.Width / 2 - 250, 50), Color.White);
 					}
 				}
 				spriteBatch.DrawString(font, "Chosen kid: ", new Vector2(75, 50), Color.White);
-				chosenAnimationTimer += gameTime.ElapsedGameTime.TotalSeconds;
+				if (gameState == GameStateSecurity.Play) 
+				{
+					spriteBatch.DrawString(font, "Current kid: " + (childCounter + 1), new Vector2(75, 150), Color.White);
+				}
+
+				/*chosenAnimationTimer += gameTime.ElapsedGameTime.TotalSeconds;
 				if (chosenAnimationTimer >= .66)
 				{
 					chosenAnimationFrame++;
@@ -407,7 +471,7 @@ namespace CIS598Project.Rooms
 						chosenAnimationFrame = 0;
 					}
 					chosenAnimationTimer -= .66;
-				}
+				}*/
 
 				spriteBatch.Draw(bodies[((int)chosenChild.color * 3) + chosenAnimationFrame], new Vector2(500, 50), Color.White);
 				spriteBatch.Draw(shirts[((int)chosenChild.shirtColor * 3) + chosenAnimationFrame], new Vector2(500, 50), Color.White);
@@ -449,7 +513,7 @@ namespace CIS598Project.Rooms
 						spriteBatch.Draw(bands[(int)children[currentRound - 1][childCounter].bandColor * 2], childPosition, Color.White);
 					}
 					spriteBatch.Draw(hands[((int)children[currentRound - 1][childCounter].color * 3) + childAnimationFrame], childPosition, Color.White);
-					if (chosenAnimationFrame == 0 || chosenAnimationFrame == 2)
+					if (childAnimationFrame == 0 || childAnimationFrame == 2)
 					{
 						spriteBatch.Draw(eyes[(int)children[currentRound - 1][childCounter].eyeColor], childPosition, Color.White);
 					}
@@ -464,22 +528,165 @@ namespace CIS598Project.Rooms
 			{
 				spriteBatch.Draw(signs[0], new Vector2(graphics.Viewport.Width / 2 + 150, graphics.Viewport.Height / 2), Color.White);
 				spriteBatch.Draw(signs[1], new Vector2(graphics.Viewport.Width / 2 + 150, graphics.Viewport.Height / 2 + 150), Color.White);
-				spriteBatch.DrawString(font, "Your guess: " + guess.ToString(), new Vector2(graphics.Viewport.Width / 2 - 350, graphics.Viewport.Height / 2 + 100), Color.White);
+				spriteBatch.DrawString(font, "Your guess: " + (guess + 1), new Vector2(graphics.Viewport.Width / 2 - 350, graphics.Viewport.Height / 2 + 100), Color.White);
+				spriteBatch.Draw(signs[2], new Vector2(graphics.Viewport.Width / 2 + 350, graphics.Viewport.Height / 2 + 100), Color.White);
+			}
+
+			if (gameState == GameStateSecurity.Results) 
+			{
+				resultsTimer += gameTime.ElapsedGameTime.TotalSeconds;
+				if (resultsTimer <= 2)
+				{
+					spriteBatch.DrawString(font, "YOU GUESSED...", new Vector2(graphics.Viewport.Width / 2 - 150, graphics.Viewport.Height / 2 - 100), Color.White);
+				}
+				else 
+				{
+					if (fail)
+					{
+						spriteBatch.DrawString(font, "INCORRECTLY!!!", new Vector2(graphics.Viewport.Width / 2 - 150, graphics.Viewport.Height / 2 - 100), Color.Red);
+						if (result == null)
+						{
+							result = boo.CreateInstance();
+							result.Play();
+						}
+						if (result.State != SoundState.Playing)
+						{
+							result = null;
+							gameState = GameStateSecurity.GameOver;
+							resultsTimer = 0;
+						}
+					}
+					else 
+					{
+						spriteBatch.DrawString(font, "CORRECTLY!!!", new Vector2(graphics.Viewport.Width / 2 - 150, graphics.Viewport.Height / 2 - 100), Color.Green);
+						if (result == null)
+						{
+							result = yay.CreateInstance();
+							result.Play();
+						}
+						if (result.State != SoundState.Playing)
+						{
+							result = null;
+							gameState = GameStateSecurity.Intermission;
+							score += 250;
+							currentRound++;
+							if (currentRound == 5) 
+							{
+								gameState = GameStateSecurity.Win;
+							}
+							resultsTimer = 0;
+						}
+					}
+				}
+			}
+
+			if (gameState == GameStateSecurity.Win) 
+			{
+				resultsTimer += gameTime.ElapsedGameTime.TotalSeconds;
+				if (resultsTimer >= 1) 
+				{
+					showText = !showText;
+					resultsTimer -= 1;
+				}
+				if (showText)
+				{
+					spriteBatch.DrawString(font, "YOU WIN!!!", new Vector2(graphics.Viewport.Width / 2 - 150, graphics.Viewport.Height / 2 - 100), Color.Green);
+				}
+				if (airHorn == null)
+				{
+					MediaPlayer.Stop();
+					airHorn = horn.CreateInstance();
+					airHorn.Play();
+				}
+				if (airHorn.State != SoundState.Playing)
+				{
+					playerRef.ticketAmount += score;
+					if (playerRef.foundSecret[15])
+					{
+						game.Exit();
+					}
+					else 
+					{
+						gameState = GameStateSecurity.Secret;
+					}
+				}
+			}
+
+			if (gameState == GameStateSecurity.Secret) 
+			{
+				fredTimer += gameTime.ElapsedGameTime.TotalSeconds;
+
+				resultsTimer += gameTime.ElapsedGameTime.TotalSeconds;
+				if (resultsTimer >= 1)
+				{
+					showText = !showText;
+					resultsTimer -= 1;
+				}
+				if (showText)
+				{
+					spriteBatch.DrawString(font, "YOU WIN!!!", new Vector2(graphics.Viewport.Width / 2 - 150, graphics.Viewport.Height / 2 - 100), Color.Green);
+				}
+
+				if (fredTimer >= 3 && fredCount > 0)
+				{
+					fredCount--;
+					fredTimer -= 3;
+				}
+
+				if (fredCount == 0)
+				{
+					purpleDialogue = true;
+				}
+
+				spriteBatch.Draw(Fred[fredCount], Vector2.Zero, Color.White);
+
+
+			}
+
+			if (gameState == GameStateSecurity.GameOver)
+			{
+				resultsTimer += gameTime.ElapsedGameTime.TotalSeconds;
+				if (resultsTimer >= 1)
+				{
+					showText = !showText;
+					resultsTimer -= 1;
+				}
+				if (showText)
+				{
+					spriteBatch.DrawString(font, "GAME OVER!!!", new Vector2(graphics.Viewport.Width / 2 - 150, graphics.Viewport.Height / 2 - 100), Color.Red);
+				}
+				if (result == null)
+				{
+					result = failed.CreateInstance();
+					result.Play();
+				}
+				if (result.State != SoundState.Playing)
+				{
+					playerRef.ticketAmount += score;
+					game.Exit();
+				}
+
+
 			}
 
 			if (gameState == GameStateSecurity.Start || gameState == GameStateSecurity.Intermission)
 			{
+				if (gameState == GameStateSecurity.Intermission && MediaPlayer.State != MediaState.Playing) 
+				{
+					MediaPlayer.Play(backgroundMusic,currentPosition);
+				}
+
 				buffer += gameTime.ElapsedGameTime.TotalSeconds;
 
-				if (buffer <= 1)
+				if (buffer <= 2)
 				{
 					spriteBatch.DrawString(font, "ROUND " + currentRound + "!!!", new Vector2(graphics.Viewport.Width / 2 - 150, graphics.Viewport.Height / 2 - 100), Color.White);
 				}
-				else if (buffer > 1 && buffer < 2)
+				else if (buffer > 2 && buffer < 4)
 				{
 					spriteBatch.DrawString(font, "READY!!!", new Vector2(graphics.Viewport.Width / 2 - 150, graphics.Viewport.Height / 2 - 100), Color.White);
 				}
-				else if (buffer >= 2 && buffer < 3)
+				else if (buffer >= 4 && buffer < 6)
 				{
 					spriteBatch.DrawString(font, "GO!!!", new Vector2(graphics.Viewport.Width / 2 - 150, graphics.Viewport.Height / 2 - 100), Color.White);
 				}
@@ -491,13 +698,43 @@ namespace CIS598Project.Rooms
 				}
 			}
 
-			spriteBatch.DrawString(font, "Score: " + score, new Vector2(graphics.Viewport.Width - 450, 50), Color.White);
+			spriteBatch.DrawString(font, "Score: " + score, new Vector2(graphics.Viewport.Width - 550, 50), Color.White);
 
 			
 
 			spriteBatch.Draw(Screens[0], Vector2.Zero, Color.White);
 			spriteBatch.Draw(Screens[1], Vector2.Zero, Color.White);
 			spriteBatch.Draw(Screens[2], Vector2.Zero, Color.White);
+
+			if (purpleDialogue && crash == false)
+			{
+				ourpleTimer += gameTime.ElapsedGameTime.TotalSeconds;
+				spriteBatch.DrawString(font, ourple[ourpleCount], new Vector2(graphics.Viewport.Width / 2 - 350, 250), Color.Purple);
+				if (ourpleTimer > 10)
+				{
+					ourpleCount++;
+					ourpleTimer -= 10;
+				}
+				if (ourpleCount == ourple.Length)
+				{
+					crash = true;
+				}
+			}
+
+			if (crash == true)
+			{
+				spriteBatch.Draw(crashScreen, Vector2.Zero, Color.White);
+				if (crashing == null)
+				{
+					crashing = Crash.CreateInstance();
+					crashing.Play();
+				}
+				if (crashing.State != SoundState.Playing)
+				{
+					playerRef.foundSecret[15] = true;
+					game.Exit();
+				}
+			}
 
 
 			spriteBatch.End();
--- CIS598Project/obj/Debug/net8.0-windows/CIS598Project.AssemblyInfo.cs
+++ CIS598Project/obj/Debug/net8.0-windows/CIS598Project.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CIS598Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6e3ee4a7b6317f2b40b9644272caed338c22a62c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d9b00f93496915ff39b415b8da387ad61ad193a")]
 [assembly: System.Reflection.AssemblyProductAttribute("CIS598Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CIS598Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
