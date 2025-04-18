76f826413e074bdbdf0c65fc38f52cba68c420b6
Bryan Gross <111697373+BryanGross21@users.noreply.github.com>
2025-04-18 18:43:21 -0500

    Added fishing minigame assets

--- CIS598Project/obj/Debug/net8.0-windows/CIS598Project.AssemblyInfo.cs
+++ CIS598Project/obj/Debug/net8.0-windows/CIS598Project.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CIS598Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+35fd8c80dce751b4c5f7621e71f981a08fd4443a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b6b2a4b1142c488f606db4d6f648f2eebe1d2160")]
 [assembly: System.Reflection.AssemblyProductAttribute("CIS598Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CIS598Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
